bc278a41f325061d57d29b8bc95e82b5b89baf66
lazysteven <playzzsteven@gmail.com>
2024-11-03 23:01:46 -0500

    Initial Commit

---
+++ fabric-like/src/main/java/me/lazysteven/fabriclike/ExampleModFabricLike.java
@@ -0,0 +1,10 @@
+package me.lazysteven.fabriclike;
+
+import me.lazysteven.ExampleMod;
+
+public final class ExampleModFabricLike {
+    public static void init() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
